5fff361da1b9c16f51a713b30d13bd8593d35426
Tyler Matteson <tyler@agritheory.com>
2024-11-28 13:08:23 -0500

    wip: add more install steps

--- fleet/install.py
+++ fleet/install.py
@@ -1,13 +1,21 @@
 # Copyright (c) 2024, AgriTheory and contributors
 # For license information, please see license.txt
 
-import frappe
+import os
+import subprocess
+import types
+
 import pycountry
+
+import frappe
+from frappe.installer import update_site_config
 
 
 def after_install():
 	install_states_and_provinces()
 	install_custom_html_blocks()
+	create_traccar_user()
+	add_custom_queue()
 
 
 def install_states_and_provinces():
@@ -43,3 +51,76 @@ def install_custom_html_blocks():
 		bv = frappe.new_doc("Custom HTML Block")
 		bv.update(battery_voltage)
 		bv.save()
+
+
+def create_traccar_user():
+	def _bypass(*args, **kwargs):
+		return
+
+	if not frappe.db.exists("User", "Traccar"):
+		u = frappe.new_doc("User")
+		u.email = "Traccar@agritheory.dev"
+		u.username = "traccar"
+		u.first_name = "Traccar"
+		u._validate_data_fields = types.MethodType(_bypass, u)
+		u.save()
+		frappe.model.rename_doc.rename_doc(
+			"User", "Traccar@agritheory.dev", "Traccar", force=1, validate=False
+		)
+		frappe.db.set_value("User", "Traccar", "email", "Traccar", update_modified=False)
+
+
+def get_user_confirmation():
+	while True:
+		user_input = (
+			input(
+				"Adding custom queue for Traccar. This requires to run 'bench setup supervisor', do you want to run it? (yes/no): "
+			)
+			.strip()
+			.lower()
+		)
+		if user_input in ["yes", "y"]:
+			return True
+		elif user_input in ["no", "n"]:
+			return False
+		else:
+			print("Please enter 'yes' or 'no'.")
+
+
+def add_custom_queue():
+	sites_path = os.getcwd()
+	common_site_config_path = os.path.join(sites_path, "common_site_config.json")
+	workers = frappe.conf.workers
+
+	if workers and "traccar" in workers.keys():
+		return
+
+	if workers:
+		workers["traccar"] = {"timeout": 8000}
+	else:
+		workers = {"traccar": {"timeout": 8000}}
+
+	update_site_config("workers", workers, validate=False, site_config_path=common_site_config_path)
+
+	# skip supervisor setup on development setups
+	if not (frappe.conf.restart_supervisor_on_update or frappe.conf.restart_systemd_on_update):
+		return
+
+	if not get_user_confirmation():
+		print("Please run 'bench setup supervisor' manually.")
+		return
+
+	process = subprocess.Popen(
+		"bench setup supervisor --yes",
+		shell=True,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+		text=True,
+	)
+	stdout, stderr = process.communicate()
+
+	if process.returncode != 0:
+		if "INFO: A newer version of bench is available" not in stderr:
+			print(f"Command failed: {stderr}.")
+		else:
+			print(f"Command failed: {stdout}.")
