31187e679a694a867a04a36b698b1dde59c28772
Ishwarya <ishwarya.m@promantia.com>
2025-07-30 17:58:45 +0530

    Integrate with Timesheet - Refactored the method

--- fleet/fleet/overrides/timesheet.py
+++ fleet/fleet/overrides/timesheet.py
@@ -9,60 +9,60 @@ import frappe
 @frappe.whitelist()
 def fetch_timesheet_from_vehicle_log(employee, start_date, end_date):
 	# Fetch all vehicle logs for the employee with geofence entered, ordered by time
-	vehicle_logs = frappe.db.sql(
-		"""
-		SELECT name, employee, creation, geofences_entered, geofences_exited
-		FROM `tabVehicle Log`
-		WHERE employee = %s
-		  AND (
-				(geofences_entered IS NOT NULL AND geofences_entered != '')
-			 OR (geofences_exited IS NOT NULL AND geofences_exited != '')
-		  )
-		  AND creation BETWEEN %s AND %s
-		ORDER BY creation ASC
-		""",
-		(employee, start_date, end_date),
-		as_dict=1,
+	vehicle_logs = frappe.get_all(
+		"Vehicle Log",
+		fields=["name", "employee", "creation", "geofences_entered", "geofences_exited"],
+		filters={
+			"employee": employee,
+			"creation": ["between", [start_date, end_date]],
+		},
+		order_by="creation asc",
 	)
+	# Filter out logs where both geofences_entered and geofences_exited are empty or None
+	vehicle_logs = [
+		log
+		for log in vehicle_logs
+		if (
+			log.get("geofences_entered") not in (None, "") or log.get("geofences_exited") not in (None, "")
+		)
+	]
 
 	fmt = "%Y-%m-%d %H:%M:%S"
 	results = []
-	i = 0
-	n = len(vehicle_logs)
-	while i < n:
-		log = vehicle_logs[i]
-		location = log["geofences_entered"]
-		entered_on = log["creation"]
-		if location:
-			j = i + 1
-			exited_on = None
-			while j < n:
-				next_log = vehicle_logs[j]
-				if next_log["geofences_exited"] == location and next_log["creation"] is not None:
-					exited_on = next_log["creation"]
-					break
-				j += 1
+	entered_dict = {}
+	for log in vehicle_logs:
+		location_entered = log.get("geofences_entered")
+		location_exited = log.get("geofences_exited")
+		creation = log.get("creation")
+		# Track entered locations
+		if location_entered:
+			entered_dict[location_entered] = {
+				"vehicle_log": log["name"],
+				"entered_on": creation,
+				"activity_type": frappe.db.get_value("Location", location_entered, "default_activity_type"),
+			}
+		# If exited and previously entered, pair and calculate hours
+		if location_exited and location_exited in entered_dict:
+			entered_data = entered_dict.pop(location_exited)
+			entered_on = entered_data["entered_on"]
+			exited_on = creation
 			hours = None
-			if exited_on:
-				entered = entered_on if isinstance(entered_on, str) else entered_on.strftime(fmt)
-				exited = exited_on if isinstance(exited_on, str) else exited_on.strftime(fmt)
-				try:
-					entered_dt = datetime.strptime(entered, fmt)
-					exited_dt = datetime.strptime(exited, fmt)
-					hours = (exited_dt - entered_dt).total_seconds() / 3600.0
-				except Exception:
-					hours = None
+			entered = entered_on if isinstance(entered_on, str) else entered_on.strftime(fmt)
+			exited = exited_on if isinstance(exited_on, str) else exited_on.strftime(fmt)
+			try:
+				entered_dt = datetime.strptime(entered, fmt)
+				exited_dt = datetime.strptime(exited, fmt)
+				hours = (exited_dt - entered_dt).total_seconds() / 3600.0
+			except Exception:
+				hours = None
 			results.append(
 				{
-					"vehicle_log": log["name"],
-					"location": location,
-					"activity_type": frappe.db.get_value("Location", location, "default_activity_type"),
+					"vehicle_log": entered_data["vehicle_log"],
+					"location": location_exited,
+					"activity_type": entered_data["activity_type"],
 					"entered_on": entered_on,
 					"exited_on": exited_on,
 					"hours": hours,
 				}
 			)
-			i = j if exited_on else i + 1
-		else:
-			i += 1
 	return results
